e9ea8280fe897c5ae6064d59ba6d10d0d44573e5
Jyotiramaya Biswal <jyotiramaya@example.com>
2026-05-03 19:01:50 +0530

    1st version of tile-shp

---
+++ main.py
@@ -0,0 +1,96 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
+
+from database import engine, SessionLocal
+from models import Product, Base
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+def home(request: Request):
+
+    db = SessionLocal()
+
+    products = db.query(Product).all()
+
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "products": products
+        }
+    )
+
+
+@app.get("/product/{product_id}")
+def product_page(request: Request, product_id: int):
+
+    db = SessionLocal()
+
+    product = db.query(Product).filter(Product.id == product_id).first()
+
+    return templates.TemplateResponse(
+        "product.html",
+        {
+            "request": request,
+            "product": product
+        }
+    )
+
+
+@app.get("/contact")
+def contact(request: Request):
+
+    return templates.TemplateResponse(
+        "contact.html",
+        {
+            "request": request
+        }
+    )
+
+
+@app.get("/admin")
+def admin_page(request: Request):
+
+    return templates.TemplateResponse(
+        "admin.html",
+        {
+            "request": request
+        }
+    )
+
+
+@app.post("/add-product")
+def add_product(
+    name: str = Form(...),
+    price: str = Form(...),
+    image: str = Form(...),
+    description: str = Form(...)
+):
+
+    db = SessionLocal()
+
+    new_product = Product(
+        name=name,
+        price=price,
+        image=image,
+        description=description
+    )
+
+    db.add(new_product)
+
+    db.commit()
+
+    return RedirectResponse(
+        url="/",
+        status_code=303
+    )
---
+++ models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Product(Base):
+
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    name = Column(String)
+
+    price = Column(String)
+
+    image = Column(String)
+
+    description = Column(String)
